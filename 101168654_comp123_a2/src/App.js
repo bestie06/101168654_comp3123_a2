13a046e23ff4dd83fddaac52f370be493d463437
bestie06 <fatih.kgokmen@gmail.com>
2020-12-01 13:58:36 -0500

    Added API key and Bootstrap import

--- 101168654_comp123_a2/src/App.js
+++ 101168654_comp123_a2/src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import "weather-icons/css/weather-icons.min.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Wheather from './app_component/weather.component';
+
+
+const API_key="69bcf12350649732b62292b9d63ad7da"
+
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state={
+      city:undefined,
+      country:undefined,
+      main:undefined,
+      icon:undefined,
+      celcius:undefined,
+      temp_max:undefined,
+      temp_min:undefined,
+      description:"",
+      error:false
+    }
+
+    this.getWeather()
+  }
+
+  getWeather=async()=>{
+    const api_call=await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Toronto,ont&appid=${API_key}`);
+    const responde = await api_call.json();
+
+    console.log(responde);
+    this.setState({
+      city:responde.name,
+      country:responde.sys.country
+    })
+  };
+  render(){
+    return(
+      <div className="App">
+      <Wheather city={this.state.city} country={this.state.country}/>
     </div>
-  );
+    );
+  }
 }
+
+
 
 export default App;
