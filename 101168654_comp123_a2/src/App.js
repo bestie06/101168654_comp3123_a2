b5ff69c91a0d57bf0f9d40f21d95185fff603da6
bestie06 <fatih.kgokmen@gmail.com>
2020-12-01 20:33:39 -0500

    getting value updated and orginazed the codes for UI

--- 101168654_comp123_a2/src/App.js
+++ 101168654_comp123_a2/src/App.js
@@ -76,29 +76,30 @@ class App extends React.Component{
     e.preventDefault();
     const city=e.target.elements.city.value;
     const country=e.target.elements.country.value;
+    if(city && country){
+      const api_call=await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_key}`);
+      const response = await api_call.json();
+  
+      console.log(response);
+      this.setState({
+        city:`${response.name},${response.sys.country}`,
+        country:response.sys.country,
+        celcius:this.calCelsius(response.main.temp),
+        temp_max:this.calCelsius(response.main.temp_max),
+        temp_min:this.calCelsius(response.main.temp_min),
+        description:response.weather[0].description,
+        
+      });
+      this.get_WeatherIcon(this.weatherIcon,response.weather[0].id);
+    }else {
+      this.setState({error:true})
+    };
 
-
-
-
-    const api_call=await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_key}`);
-    const response = await api_call.json();
-
-    console.log(response);
-    this.setState({
-      city:response.name,
-      country:response.sys.country,
-      celcius:this.calCelsius(response.main.temp),
-      temp_max:this.calCelsius(response.main.temp_max),
-      temp_min:this.calCelsius(response.main.temp_min),
-      description:response.weather[0].description,
-      
-    });
-    this.get_WeatherIcon(this.weatherIcon,response.weather[0].id)
   };
   render(){
     return(
       <div className="App">
-      <Form loadweather={this.getWeather}/>
+      <Form loadweather={this.getWeather} error={this.state.error}/>
       <Wheather city={this.state.city} country={this.state.country} 
       temp_celsius={this.state.celcius} 
       temp_max={this.state.temp_max}
