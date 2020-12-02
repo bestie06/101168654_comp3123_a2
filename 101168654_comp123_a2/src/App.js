e589852eeed9c1d13468ec3e8d5c8cd1fb36cc7d
bestie06 <fatih.kgokmen@gmail.com>
2020-12-01 22:00:30 -0500

    i have made some change the code doesnt get the value yer

--- 101168654_comp123_a2/src/App.js
+++ 101168654_comp123_a2/src/App.js
@@ -22,7 +22,7 @@ class App extends React.Component{
       temp_min:undefined,
       description:"",
       error:false
-    }
+    };
 
     //this.getWeather()
     this.weatherIcon={
@@ -34,7 +34,7 @@ class App extends React.Component{
       Clear:"wi-day-sunny",
       Clouds:"wi-day-fog"
 
-    }
+    };
   }
   calCelsius(temp){
     let cell=Math.floor(temp-273.15);
@@ -72,36 +72,44 @@ class App extends React.Component{
 
   }
 
-  getWeather=async(e)=>{
+  getWeather=async e =>{
     e.preventDefault(); 
+    
     const city=e.target.elements.city.value;
     const country=e.target.elements.country.value;
+    
     if(city && country){
       const api_call=await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_key}`);
       const response = await api_call.json();
   
       console.log(response);
-      
+
       this.setState({
         city:`${response.name},${response.sys.country}`,
         country:response.sys.country,
+        main:response.weather[0].main,
         celcius:this.calCelsius(response.main.temp),
         temp_max:this.calCelsius(response.main.temp_max),
         temp_min:this.calCelsius(response.main.temp_min),
         description:response.weather[0].description,
+        error:false
         
       });
       this.get_WeatherIcon(this.weatherIcon,response.weather[0].id);
+
+      
     }else {
-      this.setState({error:true})
-    };
+      this.setState({error:true});
+    }
 
   };
   render(){
     return(
       <div className="App">
       <Form loadweather={this.getWeather} error={this.state.error}/>
-      <Wheather city={this.state.city} country={this.state.country} 
+      <Wheather 
+      cityname={this.state.city} 
+      country={this.state.country} 
       temp_celsius={this.state.celcius} 
       temp_max={this.state.temp_max}
       temp_min={this.state.temp_min}
